89961ec8495d08bf46fbbba6d313bbf0c36799f1
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-02-06 13:14:24 +0600

    got instruction about contact page

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+/**
+ * reference contact page https://carcareful.peacefulqode.in/contact-us/
+ * do not make the img section start from below that 
+ * try to follow design from this website 
+ * all headings should not be uppercase 
+ * design with bootstrap classes 
+ * use react icons
+ * google map screenshot attach
+ */
 
 function ContactPage() {
   return (
-    <div>ContactPage</div>
-  )
+    <div>
+      <section className="section-mt">
+        <p className="text-uppercase text-primary fw-bold">Contact Us</p>
+        <h2 className="fw-bold">Contact Us Drive Crystal Clear</h2>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default ContactPage
+export default ContactPage;
+
