bddb69182dbdc5db6ff1e96382f1620013a5e4e3
Md Shayon <mdshayon0@gmail.com>
2025-05-01 11:26:25 +0600

    Incomplete service category

--- client/src/app/admin/service/service-category/page.tsx
+++ client/src/app/admin/service/service-category/page.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-function ServiceCateogyPage() {
+function ServiceCategoryPage() {
   return (
-    <div>ServiceCateogyPage</div>
+    <div>ServiceCategoryPage</div>
   )
 }
 
-export default ServiceCateogyPage
+export default ServiceCategoryPage
+
+// import styles from "./service-category.module.scss";
+// import { getQueryClient } from "@/lib/get-query-client";
+// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import { vehicleTypesOptions } from "@/app/_requests/vehicle-types";
+// import ServiceCategoryMain from "@/components/service/vehicle-type/ServiceCategoryMain";
+
+// async function ServiceCategoryPage() {
+
+//   const queryClient = getQueryClient()
+
+//   await  queryClient.prefetchQuery(vehicleTypesOptions);
+
+//   return (
+//     <div className={styles.serviceContainer}>
+//       <h1 className={styles.title}>Manage Vehicle Type</h1>      
+//       {/* Service List */}
+//       <HydrationBoundary state={dehydrate(queryClient)}>
+//         <ServiceCategoryMain styles={styles} />
+//       </HydrationBoundary>
+      
+//     </div>
+//   );
+// }
+
+// export default ServiceCategoryPage;
